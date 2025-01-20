712025d4a9b8ed7650d0ada52ff3b7ec4c7f6ca5
David Oluwanjoba <Doluwanjoba5@gmail.com>
2025-01-20 22:57:05 +0100

    Question1

---
+++ src/NestedLoop.java
@@ -0,0 +1,14 @@
+public class NestedLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) { // Outer loop for rows
+            for (int j = 0; j < 15; j++) { // Inner loop for columns
+                if (j < 5 || j >= 10) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("=");
+                }
+            }
+            System.out.println(); // Move to the next row
+        }
+    }
+}
